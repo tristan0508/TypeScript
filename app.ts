b889669dc07d61207be6c47732d6c64a26ff3636
Tristanwyatt <tristan7306@gmail.com>
2020-12-12 21:32:53 -0500

    typescript basic types

--- app.ts
+++ app.ts
@@ -1,33 +1,15 @@
-type Cominable = number | string;
-type ConvertDescript = 'as-number' | 'as-text';
-type User = { name: string; age: number };
-const u1: User = { name: 'Tristan', age: 29 };
-console.log(u1);
+let userInput: unknown;
+let userName: string;
 
+userInput = 5;
+userInput = 'Tristan';
+if (typeof userInput === 'string') 
+{
+    userName = userInput;
+}
 
-const combine = (
-    input1: Cominable,
-    input2: Cominable,
-    resultType: ConvertDescript
-    ) => {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number')
-    {
-        result = +input1 + +input2;
-    }
-    else
-    {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-};
+const generateError = (message: string, code: number) => {
+    throw {message: message, errorCode: code};
+}
 
-const combinedAges = combine(30, 27, 'as-number');
-console.log(combinedAges);
-
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-
-const combineNames = combine('Tristan', 'Kayla', 'as-text');
-console.log(combineNames);
-
+generateError('An error occured!', 500);
